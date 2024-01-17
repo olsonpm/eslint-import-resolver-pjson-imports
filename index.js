2510b7e1d306a046df14b51584d81955d3a2d761
Phil Olson <olsonpm@users.noreply.github.com>
2024-01-17 11:48:55 -0600

    support asterisk in middle

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const path = require('path')
 const interfaceVersion = 2
 
 function resolve(source, file) {
-  if (!source.startsWith('#')) return { found: false }
+  if (!source.startsWith('#')) {
+    return { found: false }
+  }
 
   const pjsonFpath = pkgUp.sync({ cwd: path.dirname(file) }) || {}
   const pjsonDir = path.dirname(pjsonFpath)
@@ -13,22 +15,29 @@ function resolve(source, file) {
 
   if (imports[source]) {
     const resolvedPath = path.resolve(pjsonDir, imports[source])
-    if (!exists(resolvedPath)) return { found: false }
+    if (!exists(resolvedPath)) {
+      return { found: false }
+    }
     return { found: true, path: resolvedPath }
   }
 
   const importKeys = Object.keys(imports)
-    .filter(key => key.endsWith('*'))
-    .map(key => key.slice(0, -1))
-    .filter(keyWithoutAsterisk => source.startsWith(keyWithoutAsterisk))
+    .filter(key => key.includes('*'))
+    .map(key => {
+      const keyStart = key.slice(0, key.indexOf('*'))
+      return [key, keyStart]
+    })
+    .filter(([, keyStart]) => source.startsWith(keyStart))
 
-  for (const key of importKeys) {
-    const replaceVal = source.slice(key.length)
-    const aliasValue = imports[key + '*']
-    const pathFromPjson = aliasValue.replace('*', replaceVal)
+  for (const [key, keyStart] of importKeys) {
+    const [pathStart, pathEnd] = imports[key].split('*')
+    const pathMiddle = source.slice(keyStart.length)
+    const pathFromPjson = pathStart + pathMiddle + pathEnd
     const resolvedPath = path.resolve(pjsonDir, pathFromPjson)
 
-    if (exists(resolvedPath)) return { found: true, path: resolvedPath }
+    if (exists(resolvedPath)) {
+      return { found: true, path: resolvedPath }
+    }
   }
 
   return { found: false }
